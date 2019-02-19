6d41839ba317c9ce2bc5f2d8da1549af27873470
nishatoma <nishatoma@gmail.com>
2019-02-18 20:36:52 -0800

    Subscribe params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +15,15 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+    // The params directly doesnt use snapshot, good for dynamically
+    // updating route content.
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'],
+        this.user.name = params['name'];
+      }
+    );
   }
 
 }
