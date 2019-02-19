05a53c6b09e019d7bd7d0d53628b589712214cc1
nishatoma <nishatoma@gmail.com>
2019-02-18 20:15:27 -0800

    navigation example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,20 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  reloadPage() {
+    // '/servers' is absolute, 'servers' is relative
+    // When we use 'servers' alone, nothing happens either.
+    // ActivatedRoute simply means the route we are currently on
+    // if u uncomment code below, app will crash.
+    // this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
