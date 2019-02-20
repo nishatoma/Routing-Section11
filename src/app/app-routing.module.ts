875e74a2b46944ef8a54c119d4988e651cf5ca8d
nishatoma <nishatoma@gmail.com>
2019-02-19 17:03:26 -0800

    Move routes to AppRoutingModule.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { UserComponent } from "./users/user/user.component";
+import { ServersComponent } from "./servers/servers.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+    // Each route is just a javascript object in this array.
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UsersComponent, children: [
+      // Any users/1, or users/2 the ':id' is dynamic now.
+      { path: ':id/:name', component: UserComponent },
+    ] },
+    // We can have nested routes using the 'children' property.
+    // The child routes need a SEPARATE server-outlet!
+    { path: 'servers', component: ServersComponent, children: [
+      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id', component: ServerComponent },
+    ] },
+    {path: 'not-found', component: PageNotFoundComponent},
+    // The ** route covers all routes other than the ones we defined! 
+    {path: '**', redirectTo: '/not-found'}, 
+  ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+// This module will encapsulate all our routes.
+export class AppRoutingModule {
+    
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,24 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  // Each route is just a javascript object in this array.
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-    // Any users/1, or users/2 the ':id' is dynamic now.
-    { path: ':id/:name', component: UserComponent },
-  ] },
-  // We can have nested routes using the 'children' property.
-  // The child routes need a SEPARATE server-outlet!
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id/edit', component: EditServerComponent },
-    { path: ':id', component: ServerComponent },
-  ] },
-  {path: 'not-found', component: PageNotFoundComponent},
-  // The ** route covers all routes other than the ones we defined! 
-  {path: '**', redirectTo: '/not-found'}, 
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -47,7 +29,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
