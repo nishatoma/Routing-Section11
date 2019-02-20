11fabf7b526ec109c9ca7dc495d93fa7d3413e47
nishatoma <nishatoma@gmail.com>
2019-02-19 18:07:37 -0800

    Add ServerResolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { ServerComponent } from "./servers/server/server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
     // Each route is just a javascript object in this array.
@@ -25,10 +27,11 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard], 
     component: ServersComponent, 
     children: [
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
-      { path: ':id', component: ServerComponent },
     ] },
-    {path: 'not-found', component: PageNotFoundComponent},
+    // {path: 'not-found', component: PageNotFoundComponent},
+    {path: 'not-found', component: ErrorPageComponent, data: {message: 'page not found!'}},
     // The ** route covers all routes other than the ones we defined! 
     {path: '**', redirectTo: '/not-found'}, 
   ];
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -17,21 +17,30 @@ export class ServerComponent implements OnInit {
 
   ngOnInit() {
     // Adding a + sign in front of the id makes it a number!
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-        console.log(params['id']);
+    // We commented out the code below because we are using a resolver instead!
+    //=========================================================================
+    //=========================================================================
+    // const id = +this.route.snapshot.params['id'];                          |
+    // this.server = this.serversService.getServer(id);                       |
+    // this.route.params.subscribe(                                           |
+    //   (params: Params) => {                                                |
+    //     this.server = this.serversService.getServer(+params['id']);        |
+    //     console.log(params['id']);                                         |
+    //   }                                                                    |
+    // );                                                                     |
+    // this.route.params.subscribe(                                           |
+    //   params => {                                                          |
+    //     let id = params['id'];                                             |
+    //     this.server = this.serversService.getServer(id);                   |
+    //   }                                                                    |
+    // );                                                                     |
+    //=========================================================================
+    //=========================================================================
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
       }
     );
-    // this.route.params.subscribe(
-    //   params => {
-    //     let id = params['id'];
-    //     this.server = this.serversService.getServer(id);
-    //   }
-    // );
-    // this.server = this.serversService.getServer(this.route.pa);
   }
 
   editServer() {
